23ccf5068d074c85fe1f08c9ffd4a9d3a2ec72fb
Riley Moore <riley.g.moore@ndsu.edu>
2018-05-04 11:20:02 -0500

    Added project to the repository

---
+++ Project/UAV Software - Final Project/Assets/Scripts/GeneratePlantingMarkers.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePlantingMarkers : MonoBehaviour
+{
+	// Initial planting marker and planting marker waypoint for the assigned field
+    public GameObject plantingMarker;
+	public GameObject plantingMarkerWaypoint;
+
+	// Game object that stores the planting markers and waypoints to reduce clutter
+	public Transform markerParent;
+
+	// Arrays storing the planting markers and waypoints
+	public static GameObject[] plantingMarkers = new GameObject[6675];
+	public static GameObject[] plantingMarkerWaypoints = new GameObject[6675];
+
+	// Variables necessary for the waypoint creation
+	private int count = 0;
+	private int flipMultiplier = 1;
+
+	void Start ()
+    {
+		// Set the position of the initial planting marker and its waypoint
+        plantingMarker.transform.position = new Vector3(transform.position.x + 67.82f, 
+			transform.position.y + 0.4f, transform.position.z + 6.32f);
+		plantingMarkerWaypoint.transform.position = new Vector3(transform.position.x + 67.82f, 
+			transform.position.y + 0.4f, transform.position.z + 6.32f);
+
+		// Store the position values of the initial planting marker for future use
+		float startingXPosition = plantingMarker.transform.position.x;
+		float startingYPosition = plantingMarker.transform.position.y;
+		float startingZPosition = plantingMarker.transform.position.z;
+
+		// Generate all necessary planting markers and waypoints for the assigned field
+		for (int j = 0; j < 25; j++)
+		{
+			for (int i = 0; i < 267; i++) 
+			{
+				Vector3 newPosition = new Vector3(startingXPosition, 
+					startingYPosition, startingZPosition);
+
+				plantingMarkers [count] = Instantiate (plantingMarker, newPosition, Quaternion.identity, markerParent);
+				plantingMarkerWaypoints [count] = Instantiate (plantingMarkerWaypoint, newPosition, Quaternion.identity, markerParent);
+
+				startingXPosition -= 0.51f * flipMultiplier;
+				count += 1;
+			}
+
+			startingZPosition -= 0.51f;
+			flipMultiplier *= -1;
+		}
+	}
+}
---
+++ Project/UAV Software - Final Project/Assets/Scripts/UAVHandler.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UAVHandler : MonoBehaviour 
+{
+    // Public variables
+    public GameObject UAVIdlePoint;
+
+    // Private variables
+	private int visitedWaypoints = 0;
+	private float UAVSpeed = 3.0f;
+	private GameObject target;
+	private float defaultYPosition = 1.0f;
+	private bool returningToPosition = false;
+	private Vector3 actualTarget;
+    private int currentUAVSeedCount = 199943;
+    private GUIStyle style = new GUIStyle();
+    private Vector3 idlePosition = new Vector3();
+    private bool atIdlePosition;
+
+    // Constants
+    private const int UAVSeedCapacity = 199943;
+    private const int acreSeedCapacity = 600000;
+    private const int seedsPerPlantingMarker = 89;
+
+    private void Start()
+    {
+        style.normal.textColor = Color.black;
+        idlePosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        atIdlePosition = true;
+    }
+
+    void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+        {
+            InvokeRepeating("PlantingRoutine", 0.0f, Time.deltaTime);
+        }
+	}
+
+    private void PlantingRoutine()
+    {
+        float step = UAVSpeed * Time.deltaTime;
+        if (atIdlePosition)
+        {
+            actualTarget = new Vector3(transform.position.x, defaultYPosition, transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, actualTarget, step);
+
+            if (transform.position.y == defaultYPosition)
+            {
+                atIdlePosition = false;
+            }
+        }
+
+        if (!returningToPosition && !atIdlePosition)
+        {
+            target = GeneratePlantingMarkers.plantingMarkerWaypoints[visitedWaypoints];
+            actualTarget = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, actualTarget, step);
+        }
+
+        if ((transform.position.x == target.transform.position.x)
+            && (transform.position.z == target.transform.position.z)
+            && returningToPosition == false && !atIdlePosition)
+        {
+            actualTarget = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, actualTarget, step);
+
+            returningToPosition = true;
+        }
+
+        if (returningToPosition && !atIdlePosition)
+        {
+            if (transform.position.y == actualTarget.y)
+            {
+                actualTarget = new Vector3(transform.position.x, defaultYPosition, transform.position.z);
+            }
+
+            if (transform.position.y != defaultYPosition)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, actualTarget, step);
+            }
+            else
+            {
+                returningToPosition = false;
+            }
+
+            if (returningToPosition == false)
+            {
+                GeneratePlantingMarkers.plantingMarkers[visitedWaypoints].GetComponent<Renderer>().material.color = new Color(0.0f, 255.0f, 0.0f);
+                visitedWaypoints += 1;
+                currentUAVSeedCount -= 89;
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "Current UAV Seed Count: " + currentUAVSeedCount.ToString("n0"), style);
+    }
+}
